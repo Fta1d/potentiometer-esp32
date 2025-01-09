6ba7643b4ebc1886d086624edaf60379464e0f49
Fta1d <li4nomark228@gmail.com>
2025-01-09 13:41:18 +0200

    reading data using adc oneshot mode

--- main/potentiometer-esp32.c
+++ main/potentiometer-esp32.c
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "hal/adc_types.h"
+#include "esp_adc/adc_oneshot.h"
 
-void app_main(void)
-{
+#define ADC_CHANNEL ADC_CHANNEL_3
+
+static int adc_raw[1][10];
+
+void app_main(void) {
+    adc_oneshot_unit_handle_t adc2_handle;
+    adc_oneshot_unit_init_cfg_t init_cfg = {
+        .unit_id = ADC_UNIT_2,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_cfg, &adc2_handle));
+
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_11,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC_CHANNEL, &config));
+
+    while (1) {
+        ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, ADC_CHANNEL, &adc_raw[0][0]));
+        ESP_LOGI("adc", "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_2 + 1, ADC_CHANNEL, adc_raw[0][0]);
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 
 }
