f1a6b4fec23171708a3748e7f8441a0a88d8e3ef
Fta1d <li4nomark228@gmail.com>
2025-01-21 16:31:34 +0200

    trigger solution abandoned. instead, new workaround with treshold solution.

--- main/potentiometer-esp32.c
+++ main/potentiometer-esp32.c
@@ -8,11 +8,11 @@
 #include "esp_adc/adc_cali_scheme.h"
 
 #define ADC_CHANNEL ADC_CHANNEL_3
-#define TRIGGER_PIN GPIO_NUM_15
 #define GLOBAL_ATTEN ADC_ATTEN_DB_12
 
+#define NEG_TO_POS(x) if(x<0) x=-x
+
 static QueueHandle_t evt_queue = NULL;
-static int adc_raw[1][10];
 static adc_oneshot_unit_handle_t adc2_handle;
 static adc_cali_handle_t cali_handle;
 
@@ -30,7 +30,7 @@ void adc_configure (void) {
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC_CHANNEL, &config));
 }
 
-void adc_calibration (void) {
+void adc_calibration_configure (void) {
 
     adc_cali_line_fitting_config_t cali_conf = {
         .unit_id = ADC_UNIT_2,
@@ -41,46 +41,36 @@ void adc_calibration (void) {
     ESP_ERROR_CHECK(adc_cali_create_scheme_line_fitting(&cali_conf, &cali_handle));
 }
 
-// static void IRAM_ATTR trigger_pin_isr_handler(void *arg) {
-//     int pin = (int)arg;
-//     static uint32_t last_isr_time = 0;
-//     uint32_t current_time = xTaskGetTickCountFromISR();
-//
-//     if (current_time - last_isr_time > 50 / portTICK_PERIOD_MS) {
-//         last_isr_time = current_time;
-//         xQueueSendFromISR(evt_queue, &pin, NULL);
-//     }
-// }
 
 void potentiometer_task(void* arg) {
-    int pin, voltage;
+    int voltage, current_voltage;
+    int prev_voltage = 0, offset = 128, treshold = 30;
 
     while (1) {
-            esp_err_t ret = adc_oneshot_get_calibrated_result(adc2_handle, cali_handle, ADC_CHANNEL, &voltage);
+        esp_err_t ret = adc_oneshot_get_calibrated_result(adc2_handle, cali_handle, ADC_CHANNEL, &voltage);
+        current_voltage = voltage - offset;
 
-            if (ret == ESP_OK) {
-                ESP_LOGI("adc", "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_2 + 1, ADC_CHANNEL, voltage - 128);
-            } else {
-                ESP_LOGE("adc", "Failed to read ADC data: %s", esp_err_to_name(ret));
-            }
+        if (ret == ESP_OK) {
+            int diff = current_voltage - prev_voltage;
+            NEG_TO_POS(diff);
 
-            vTaskDelay(pdMS_TO_TICKS(1000));
+            if (diff > treshold)
+                ESP_LOGI("adc", "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_2 + 1, ADC_CHANNEL, voltage - offset);
+        } else {
+            ESP_LOGE("adc", "Failed to read ADC data: %s", esp_err_to_name(ret));
+        }
+
+        prev_voltage = current_voltage;
+
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
 
 void app_main(void) {
     adc_configure();
-    adc_calibration();
-
-    // gpio_reset_pin(TRIGGER_PIN);
-    // gpio_set_direction(TRIGGER_PIN, GPIO_MODE_INPUT);
-    // gpio_set_pull_mode(TRIGGER_PIN, GPIO_PULLUP_ONLY);
-    // gpio_set_intr_type(TRIGGER_PIN, GPIO_INTR_NEGEDGE);
+    adc_calibration_configure();
 
     evt_queue = xQueueCreate(10, sizeof(int));
 
     xTaskCreate(potentiometer_task, "potentiometer_task", 2048, NULL, 10, NULL);
-
-    // gpio_install_isr_service(0);
-    // gpio_isr_handler_add(TRIGGER_PIN, trigger_pin_isr_handler, (void*)TRIGGER_PIN);
 }
